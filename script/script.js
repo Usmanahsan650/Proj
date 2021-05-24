743fe3f1bb06c7ff6f768097b742b0b780df29d7
Syed Usman Ahsan <usmanahsan650@gmail.com>
2021-05-25 01:00:35 +0500

    mini max success

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded',function(){
+    let move=String() //best
     var count=0,player1=[],player2=[];
-
+    var grid=new Array(8)
     var board=new Array(8)
     for(i=0;i<board.length;i++){
+        grid[i]=new Array(8)
         board[i]=new Array(8);
     }
     for(i=0;i<8;i++){
@@ -34,11 +36,122 @@ window.addEventListener('DOMContentLoaded',function(){
         col.setAttribute("class","box")
         row.appendChild(col);
     }
-    } 
+    }
+// class node{
+//     constructor(){
+//         this.val=0
+//         this.childrens=[]    
+//     }
+// } 
+function printgr(g){
+    for (i=0;i<8;i++)
+    {
+    for(j=0;j<8;j++)
+    console.log(g[i][j])
+    console.log("\n")
+    }
+}
+function max(pos,pos2,grid){
+    console.log("pass 1")
+        if(isTerminal(pos,pos2,grid)){
+            console.log("pass 3")
+            return utility(pos,pos2,grid)
+        }
+        else{
+            console.log("pass 2")
+            let i=pos
+            let id=[[i[0]-2,i[1]-1],[i[0]-2,i[1]+1],[i[0]+2,i[1]-1],[i[0]+2,i[1]+1],[i[0]-1,i[1]-2],[i[0]-1,i[1]+2],[i[0]+1,i[1]-2],[i[0]+1,i[1]+2]]
+            let max=new Array(2);
+            
+            max[0]=0
+            id.forEach(element => {
+                console.log(element)
+                // console.log(checkT(element,pos,grid))
+                if((element[0]>=0&&element[0]<8)&&(element[1]>=0&&element[1]<8)&&checkT(element,pos,grid)){
+                    console.log("pass 5")
+                    let t=min(element,pos2,grid)
+                    if(t[0]>max[0]){
+                        console.log(t)
+                        max=t
+                        move=element
+                        
+                    }
+                }
+                
+        });    
+        return max
+    }
+    }
+    function min(pos,pos2,grid){
+        console.log("min")
+        if(isTerminal(pos,pos2,grid)){
+            console.log(utility(pos,pos2,grid))
+            return utility(pos,pos2,grid)
+        }
+        else{
+            let i=pos2
+            let id=[[i[0]-2,i[1]-1],[i[0]-2,i[1]+1],[i[0]+2,i[1]-1],[i[0]+2,i[1]+1],[i[0]-1,i[1]-2],[i[0]-1,i[1]+2],[i[0]+1,i[1]-2],[i[0]+1,i[1]+2]]
+            let min=new Array(2);
+            min=[8,8]
 
+            id.forEach(element => {
+                if((element[0]>=0&&element[0]<8)&&(element[1]>=0&&element[1]<8)&&checkT(element,pos,grid)){
+                    let t=max(pos,element,grid)
+                
+                    if(t[0]<min[0]){
+                        console.log(t)
+                        min=t;
+                    }
+                }
+                
+            });    
+            return min;
+        }
+        }
+
+    function isTerminal(pos,pos2,grid){
+        if(!optionsT(pos,grid)){
+            return 1;
+        }
+        if(!optionsT(pos2,grid)){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    function utility(pos,pos2,grid){
+        let t=Array(2);
+        t.push(optionsT(pos,grid))
+        t.push(pos2,grid)
+        return t;
+
+    }
+//  createTree(){
+
+// }
 //logic 
+function hint(move){
+    let id=revert(move)
+    console.log(id)
+    let box=document.getElementById(id)
+    box.style.border="5px solid rgb(100, 100,190 )";
+                      
+}
 document.getElementById("hint").addEventListener("click",()=>{
-    console.log("Next step")
+    // createTree()
+    for (i=0;i<8;i++)
+    {
+    for(j=0;j<8;j++)
+    grid[i][j]=board[i][j]
+    }
+    let p1=convert(player1)
+    let p2=convert(player2)
+    console.log(max(p1,p2,grid))
+    console.log(move)
+    hint(move)
+    // printgr(board)
+
 })
     function convert(id){
         // if(typeof id =="string"){
@@ -61,7 +174,6 @@ document.getElementById("hint").addEventListener("click",()=>{
     }
     function check(id,p,flag=0){
         index=id;
-        console.log(index[0])
         if(board[index[0]][index[1]]!==0&&((Math.abs(p[0]-index[0])==1&&Math.abs(p[1]-index[1])==2)||(Math.abs(p[0]-index[0])==2&&Math.abs(p[1]-index[1])==1)))
         {   if(flag==1)
                 board[index[0]][index[1]]=0
@@ -71,6 +183,47 @@ document.getElementById("hint").addEventListener("click",()=>{
 
         return 0;
     }
+
+    function checkT(id,p,grid,flag=1){
+        index=id;
+        // printgr(grid)
+        console.log(index[0],index[1])
+        if(grid[index[0]][index[1]]!==0&&((Math.abs(p[0]-index[0])==1&&Math.abs(p[1]-index[1])==2)||(Math.abs(p[0]-index[0])==2&&Math.abs(p[1]-index[1])==1)))
+        {  if(flag==1)
+            grid[index[0]][index[1]]=0
+            console.log("valid")
+            return 1;
+        }
+        return 0;
+    }
+    function optionsT(p,grid){
+        let li=[];
+       let op=0,i=p;
+       
+        let id=[[i[0]-2,i[1]-1],[i[0]-2,i[1]+1],[i[0]+2,i[1]-1],[i[0]+2,i[1]+1],[i[0]-1,i[1]-2],[i[0]-1,i[1]+2],[i[0]+1,i[1]-2],[i[0]+1,i[1]+2]]
+       id.forEach(element => {
+           console.log(i+" "+element)
+
+           if((element[0]>=0&&element[0]<8)&&(element[1]>=0&&element[1]<8)&&checkT(element,i,grid,0))
+              {
+                  op++;
+                //   let box=document.getElementById(revert(element))
+                //  box.style.border="5px solid rgb(100, 255, 100)";
+                //  li.push(box); 
+                //  box.style.boxShadow="0 0 50px #00ff00"
+                //   box.addEventListener("click",()=>{
+                //      for(i=0;i<li.length;i++){
+                //         box=li[i];
+                //         box.style.border="";
+                //         box.style.boxShadow="";
+                //      } 
+                        
+                //   })
+              }
+       });
+       console.log(op)
+       return op;
+}
     function options(p){
             let li=[];
            let op=0,i=convert(p);
@@ -121,7 +274,8 @@ document.getElementById("hint").addEventListener("click",()=>{
             {
                
                 if(count==0){
-                    
+                    let t=convert(id)
+                    board[t[0]][t[1]]=0
                     console.log("Fdf")
                     player1=id;
                     let box=event.target;
@@ -132,8 +286,7 @@ document.getElementById("hint").addEventListener("click",()=>{
                     box.style.border="";
                     box.style.boxShadow="";
                     },1000)
-                    let t=convert(id);
-                    board[t[0]][t[1]]=0;
+                    
                 }
                 else if(check(convert(id),convert(player1),1)){
                     unsetbox(player1)
@@ -147,7 +300,7 @@ document.getElementById("hint").addEventListener("click",()=>{
                     box.style.boxShadow="";
                     },1000)
                     setbox(player2)
-                    if(options(player2)==0){
+                    if(options(player2)==0){   //player2 loses
                         let b=document.getElementById(player2)
                         b.style.backgroundColor="pink"
                         b.style.border="5px solid rgb(255, 102, 120)";
@@ -174,6 +327,8 @@ document.getElementById("hint").addEventListener("click",()=>{
                 }
             }else{
                 if(count==1){
+                    t=convert(id)
+                    board[t[0]][t[1]]=0
                     player2=id;
                     let box=event.target;
                     box.innerHTML="&#9816"
@@ -186,8 +341,7 @@ document.getElementById("hint").addEventListener("click",()=>{
                     },1000)
                     setbox(player1)
                     options(player1)
-                    let t=convert(id);
-                    board[t[0]][t[1]]=0
+                    
 
                 }
                 else if(check(convert(id),convert(player2),1)){
@@ -203,7 +357,7 @@ document.getElementById("hint").addEventListener("click",()=>{
                         box.style.boxShadow="";
                     },1000)
                     setbox(player1)
-                    if(options(player1)==0){
+                    if(options(player1)==0){   //player1 loses
                         let b=document.getElementById(player1)
                         b.style.backgroundColor="pink"
                         b.style.border="5px solid rgb(255, 102, 120)";
